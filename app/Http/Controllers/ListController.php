8a6baf6b4719f37685728dc3bd97cd932c3dfc21
Linas Zemgulys <32411184+Zemgulys@users.noreply.github.com>
2018-12-18 08:17:22 +0200

    Update ListController.php

--- app/Http/Controllers/ListController.php
+++ app/Http/Controllers/ListController.php
@@ -28,27 +28,34 @@ class ListController extends Controller
            }else{
                $page = 1;
            }
-            $options = ['http' => [
-                'method' => 'GET',
-                'header' => ['User-Agent: PHP']
-            ]
-            ];
-            $context = stream_context_create($options);
+           
+            $curl = curl_init();
 
-            $url1 = "https://api.github.com/repos/$this->repo/issues?client_id=$this->client_id&client_secret=$this->secret&per_page=$this->per_page&page=$page";
-            $issues = file_get_contents($url1, false, $context);
-            $issues = json_decode($issues, true);
-
-            $url2 = "https://api.github.com/search/issues?q=repo:$this->repo+is:issue+is:open";
-            $count = file_get_contents($url2, false, $context);
+            curl_setopt($curl, CURLOPT_URL, "https://api.github.com/repos/$this->repo/issues?client_id=$this->client_id&client_secret=$this->secret&per_page=$this->per_page&page=$page");
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($curl, CURLOPT_HEADER, false);
+            curl_setopt($curl, CURLOPT_USERAGENT, true);
+            $data = curl_exec($curl);
+            $issues = json_decode($data, true);
+            
+            curl_setopt($curl, CURLOPT_URL, "https://api.github.com/search/issues?q=repo:$this->repo+is:issue+is:open");
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($curl, CURLOPT_HEADER, false);
+            curl_setopt($curl, CURLOPT_USERAGENT, true);
+            $count = curl_exec($curl);
             $open_count = json_decode($count, true);
 
-            $url3 = "https://api.github.com/search/issues?q=repo:$this->repo+is:issue+is:closed";
-            $close = file_get_contents($url3, false, $context);
+            curl_setopt($curl, CURLOPT_URL, "https://api.github.com/search/issues?q=repo:$this->repo+is:issue+is:closed");
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($curl, CURLOPT_HEADER, false);
+            curl_setopt($curl, CURLOPT_USERAGENT, true);
+            $close = curl_exec($curl);
             $close_count = json_decode($close, true);
             
             $total_open = $open_count['total_count']; 
             $total_close = $close_count['total_count']; 
+
+            curl_close($curl);
         }
 
             return view('list')->with([
